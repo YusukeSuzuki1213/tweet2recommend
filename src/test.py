3e9b676745dc4654ab9957970e0a3a9144a0c049
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2021-01-09 13:10:57 +0900

    setup: linter adn formatter on vscode

--- src/test.py
+++ src/test.py
@@ -1,7 +1,22 @@
+import tweepy
+
+# override tweepy.StreamListener to add logic to on_status
+
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+
 def main():
-    print("test")
+    listener = MyStreamListener()
+    stream = tweepy.Stream(auth=api.auth, listener=listener)
+
+
+def auth():
+    auth = tweepy.OAuthHandler(consumer_token, consumer_secre)
 
 
 if __name__ == '__main__':
     main()
-    

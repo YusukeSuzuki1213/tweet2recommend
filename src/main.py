419d06e78368909e5ab037f3d1cb4f46476a38f0
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2021-01-09 22:52:23 +0900

    impl: Streaming

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from typing import Any
+import tweepy
+from settings import (
+    TWITTER_CONSUMER_TOKEN,
+    TWITTER_CONSUMER_SECRET,
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_SECRET,
+    FOLLOWED_USER_LIST
+)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status: Any):
+        print(status.text)
+        print(status)
+
+    def on_error(self, status_code: int):
+        if status_code == 420:
+            return False
+
+
+def get_auth() -> tweepy.OAuthHandler:
+    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_TOKEN, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+    return auth
+
+
+listener = MyStreamListener()
+auth = get_auth()
+stream = tweepy.Stream(auth=auth, listener=listener)
+stream.filter(follow=FOLLOWED_USER_LIST, is_async=True)
---
+++ src/settings.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# KEY
+TWITTER_CONSUMER_TOKEN = os.getenv('TWITTER_CONSUMER_TOKEN', None)
+TWITTER_CONSUMER_SECRET = os.getenv('TWITTER_CONSUMER_SECRET', None)
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN', None)
+TWITTER_ACCESS_SECRET = os.getenv('TWITTER_ACCESS_SECRET', None)
+
+if TWITTER_CONSUMER_TOKEN is None:
+    print('Specify TWITTER_CONSUMER_TOKEN as environment variable.')
+    os.sys.exit(1)
+
+if TWITTER_CONSUMER_SECRET is None:
+    print('Specify TWITTER_CONSUMER_SECRET as environment variable.')
+    os.sys.exit(1)
+
+if TWITTER_ACCESS_TOKEN is None:
+    print('Specify TWITTER_ACCESS_TOKEN as environment variable.')
+    os.sys.exit(1)
+
+if TWITTER_ACCESS_SECRET is None:
+    print('Specify TWITTER_ACCESS_SECRET as environment variable.')
+    os.sys.exit(1)
+
+
+# TWITTER ACCOUNT
+USER_ID_1 = os.getenv('USER_ID_1', None)
+USER_ID_2 = os.getenv('USER_ID_2', None)
+USER_ID_TEST = os.getenv('USER_ID_TEST', None)
+
+if USER_ID_1 is None:
+    print('Specify USER_ID_1 as environment variable.')
+    os.sys.exit(1)
+
+if USER_ID_2 is None:
+    print('Specify USER_ID_2 as environment variable.')
+    os.sys.exit(1)
+
+if USER_ID_TEST is None:
+    print('Specify USER_ID_TEST as environment variable.')
+    os.sys.exit(1)
+
+FOLLOWED_USER_LIST = [
+    USER_ID_1, USER_ID_2, USER_ID_TEST
+]
